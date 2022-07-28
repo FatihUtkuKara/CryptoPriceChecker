1ca9e477fe9248171407045c386160e7f404ee39
Emre Eken <emre.eken@euromsg.com>
2022-07-28 11:24:07 +0300

    bug fix for the model

--- app/src/main/java/com/example/cryptopricechecker/DetailActivity.java
+++ app/src/main/java/com/example/cryptopricechecker/DetailActivity.java
@@ -2,12 +2,16 @@ package com.example.cryptopricechecker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.cryptopricechecker.details.BaseJson2POJO;
 import com.example.cryptopricechecker.retrofit.ApiUtils;
 import com.example.cryptopricechecker.retrofit.PricesDaoInterface;
@@ -21,49 +25,52 @@ public class DetailActivity extends AppCompatActivity {
     private PricesDaoInterface detailDıf;
     ImageView imageView;
     TextView textView;
+    BaseJson2POJO cryptodetail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        //imageView = findViewById(R.id.imageView);
+        imageView = findViewById(R.id.imageView);
         textView = findViewById(R.id.textView);
         detailDıf = ApiUtils.getPricesDaoInterface();
+
         if (getIntent().hasExtra("coinName")) {
             coinName = getIntent().getStringExtra("coinName");
-            Toast.makeText(this, coinName, Toast.LENGTH_SHORT).show();
-
-
         }
-
-
         getCryptoDetail();
-
-
     }
 
 
     public void getCryptoDetail() {
-
-
-
         detailDıf.getCoinDetails("bitcoin").enqueue(new Callback<BaseJson2POJO>() {
             @Override
             public void onResponse(Call<BaseJson2POJO> call, Response<BaseJson2POJO> response) {
-
-                BaseJson2POJO cryptodetail = response.body();
-
-               textView.setText(cryptodetail.getDescription().getEn());
-               Log.d("deneme",cryptodetail.getName());
-
-                Toast.makeText(DetailActivity.this, "deneme", Toast.LENGTH_SHORT).show();
+                cryptodetail = response.body();
+                populateUI();
             }
 
             @Override
             public void onFailure(Call<BaseJson2POJO> call, Throwable t) {
-
+                Log.e("DetailActivity", t.getLocalizedMessage());
             }
         });
 
 
+    }
+
+    private void populateUI() {
+        Glide.with(getApplicationContext()).
+                load(cryptodetail.getImage().getLarge()).into(imageView);
+
+        textView.setText(cryptodetail.getDescription().getEn());
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse(cryptodetail.getLinks().getHomepage().get(0)));
+                startActivity(browserIntent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/cryptopricechecker/details/Links.java
+++ app/src/main/java/com/example/cryptopricechecker/details/Links.java
@@ -9,35 +9,8 @@ public class Links {
    @SerializedName("homepage")
    List<String> homepage;
 
-   @SerializedName("blockchain_site")
-   List<String> blockchainSite;
-
    @SerializedName("official_forum_url")
    List<String> officialForumUrl;
-
-   @SerializedName("chat_url")
-   List<String> chatUrl;
-
-   @SerializedName("announcement_url")
-   List<String> announcementUrl;
-
-   @SerializedName("twitter_screen_name")
-   String twitterScreenName;
-
-   @SerializedName("facebook_username")
-   String facebookUsername;
-
-   @SerializedName("bitcointalk_thread_identifier")
-   String bitcointalkThreadIdentifier;
-
-   @SerializedName("telegram_channel_identifier")
-   String telegramChannelIdentifier;
-
-   @SerializedName("subreddit_url")
-   String subredditUrl;
-
-   @SerializedName("repos_url")
-   ReposUrl reposUrl;
 
 
     public void setHomepage(List<String> homepage) {
@@ -47,74 +20,11 @@ public class Links {
         return homepage;
     }
     
-    public void setBlockchainSite(List<String> blockchainSite) {
-        this.blockchainSite = blockchainSite;
-    }
-    public List<String> getBlockchainSite() {
-        return blockchainSite;
-    }
-    
     public void setOfficialForumUrl(List<String> officialForumUrl) {
         this.officialForumUrl = officialForumUrl;
     }
     public List<String> getOfficialForumUrl() {
         return officialForumUrl;
-    }
-    
-    public void setChatUrl(List<String> chatUrl) {
-        this.chatUrl = chatUrl;
-    }
-    public List<String> getChatUrl() {
-        return chatUrl;
-    }
-    
-    public void setAnnouncementUrl(List<String> announcementUrl) {
-        this.announcementUrl = announcementUrl;
-    }
-    public List<String> getAnnouncementUrl() {
-        return announcementUrl;
-    }
-    
-    public void setTwitterScreenName(String twitterScreenName) {
-        this.twitterScreenName = twitterScreenName;
-    }
-    public String getTwitterScreenName() {
-        return twitterScreenName;
-    }
-    
-    public void setFacebookUsername(String facebookUsername) {
-        this.facebookUsername = facebookUsername;
-    }
-    public String getFacebookUsername() {
-        return facebookUsername;
-    }
-    
-    public void setBitcointalkThreadIdentifier(String bitcointalkThreadIdentifier) {
-        this.bitcointalkThreadIdentifier = bitcointalkThreadIdentifier;
-    }
-    public String getBitcointalkThreadIdentifier() {
-        return bitcointalkThreadIdentifier;
-    }
-    
-    public void setTelegramChannelIdentifier(String telegramChannelIdentifier) {
-        this.telegramChannelIdentifier = telegramChannelIdentifier;
-    }
-    public String getTelegramChannelIdentifier() {
-        return telegramChannelIdentifier;
-    }
-    
-    public void setSubredditUrl(String subredditUrl) {
-        this.subredditUrl = subredditUrl;
-    }
-    public String getSubredditUrl() {
-        return subredditUrl;
-    }
-    
-    public void setReposUrl(ReposUrl reposUrl) {
-        this.reposUrl = reposUrl;
-    }
-    public ReposUrl getReposUrl() {
-        return reposUrl;
     }
     
 }
